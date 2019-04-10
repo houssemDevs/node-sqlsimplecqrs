cbd24ea07f1be56d08182d15c6d04997c96c9de9
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-10 16:58:27 +0200

    p

--- src/sql/read/queryhandler.ts
+++ src/sql/read/queryhandler.ts
@@ -1,6 +1,5 @@
 import { IQueryHandler } from "node-simplecqrs";
-import { ISqlSortCriteria } from "./criteria";
 import { ISqlQuery } from "./query";
 
 export interface ISqlQueryHandler<TEntity>
-  extends IQueryHandler<TEntity, ISqlQuery<ISqlSortCriteria>> {}
+  extends IQueryHandler<TEntity, ISqlQuery> {}
